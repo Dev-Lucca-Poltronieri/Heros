1f0365945391764895b5223a8f98d8ad91f4768c
Dev-Lucca-Poltronieri <lucca41948@fiec.edu.br>
2026-06-18 14:06:43 -0300

    adicionando .env

--- node/src/config/db.js
+++ node/src/config/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    port: '3310',
-    database: 'hero'
+console.log('HOST:', process.env.DB_HOST);
+console.log('USER:', process.env.DB_USER);
+console.log('DB:', process.env.DB_NAME);
+
+const conexao = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = conexao.promise();
