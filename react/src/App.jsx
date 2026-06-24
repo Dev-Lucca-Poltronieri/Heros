28d4dc055a703fcccbaa4a51d4ced056200903d0
Dev-Lucca-Poltronieri <lucca41948@fiec.edu.br>
2026-06-24 11:32:56 -0300

    arruamndo extensao

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,7 @@
 import Formulario from './components/Formulario';
 import Cadastro from './components/Cadastro';
 import Card from './components/Card';
-import Login from './components/Login';
+import Login from './components/login';
 import Profile from './components/Profile';
 
 import axios from "axios";
