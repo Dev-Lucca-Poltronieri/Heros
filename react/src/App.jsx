76c6a1325546908b8fec614fb035da79679d260c
Dev-Lucca-Poltronieri <lucca41948@fiec.edu.br>
2026-06-24 11:37:56 -0300

    ajustando nome definitivo para Login com L maiusculo

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,7 @@
 import Formulario from './components/Formulario';
 import Cadastro from './components/Cadastro';
 import Card from './components/Card';
-import Login from './components/LoginTemporario';
+import Login from './components/Login.jsx';
 import Profile from './components/Profile';
 
 import axios from "axios";
